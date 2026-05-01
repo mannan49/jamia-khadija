1edb267eb33dabf62da32df8c4890a59154715c5
mannan49 <mannannasir49@gmail.com>
2026-05-01 16:29:52 +0500

    Update environment urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://dareecha-api.online/api/v1',
-  cloudFrontUrl: 'https://ds0gx3ls7od5o.cloudfront.net/'
+  apiBaseUrl: 'https://dareechaapi.azurewebsites.net/api/v1',
+  cloudFrontUrl: 'https://ds0gx3ls7od5o.cloudfront.net/',
 };
